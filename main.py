0678a2c4d37037856ef09857f1f6ee97beaab22e
Dung Ho <fin.dungho@gmail.com>
2020-11-29 19:47:23 +0200

    Fix Heroku deployment.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Author : Dung Ho
 # Email: fin.dungho@gmail.com
 
-import os
+
 from application import create_app
 
 
@@ -12,5 +12,4 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
